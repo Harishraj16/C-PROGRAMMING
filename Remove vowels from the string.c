f3b837dc027cd66e3d369e03a6cbab367264bff5
harish_selva <harishselvaraj27@gmail.com>
2023-12-06 16:42:09 +0530

    Update Remove vowels from the string.c

--- Remove vowels from the string.c
+++ Remove vowels from the string.c
@@ -1,3 +1,5 @@
+//Brute force approach
+
 #include<stdio.h>
 #include<string.h>
 int main(){
@@ -15,4 +17,29 @@ int main(){
     }
     printf("%s",result);
     return 0;
+}
+
+//Optimal Approach:
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+  char str[100];
+  printf("Enter the string: ");
+  scanf("%s", str);
+
+  int length = strlen(str);
+  int consonantIndex = 0;
+
+  for (int i = 0; i < length; i++) {
+    if (str[i] != 'a' && str[i] != 'e' && str[i] != 'i' && str[i] != 'o' && str[i] != 'u') {
+      str[consonantIndex++] = str[i];
+    }
+  }
+
+  str[consonantIndex] = '\0'; // Add null terminator to the end of the consonant string
+
+  printf("Consonants: %s", str);
+  return 0;
 }
