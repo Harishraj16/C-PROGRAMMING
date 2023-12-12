168298635fbdcf23f145057ea271b27894c6f5cc
harish_selva <harishselvaraj27@gmail.com>
2023-12-12 16:49:39 +0530

    Update Alphabets form A to Z.c

--- Alphabets form A to Z.c
+++ Alphabets form A to Z.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    int i;
     printf("Alphabets from A to Z: ");
-    for(i='A';i<='Z';i++){
+    for(int i='A';i<='Z';i++){
         printf(" %c",i);
     }
     printf("\nAlphabets from a to z: ");
-    for(i='a';i<='z';i++){
+    for(int i='a';i<='z';i++){
         printf(" %c",i);
     }
     return 0;
