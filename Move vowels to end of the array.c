01f1a370275a850e9e9e8073656b95cba3787fcf
harish_selva <harishselvaraj27@gmail.com>
2023-12-12 17:23:25 +0530

    Update Move vowels to end of the array.c

--- Move vowels to end of the array.c
+++ Move vowels to end of the array.c
@@ -4,12 +4,16 @@ int main(){
     char str[100];
     printf("Enter the string: ");
     scanf("%s",str);
-    int length=strlen(str)-1;
+    int length= strlen(str);
+    int final = length;  //Have a separate variable for last char because inside the loop legth variable is getting changed 
     for(int i=0;i<length;i++){
         if(str[i]=='a' || str[i]=='e' || str[i]=='i' || str[i]=='o' || str[i]=='u'){
             char temp = str[i];
-            str[i]=str[i+1];
-            str[i+1]=temp;
+            for(int j=i;j<length;j++){
+                str[j]=str[j+1];
+            }
+            str[final-1]=temp; //if here length variable is used then it vowels in the end will be in reverse order. (it is not a problem but for convention i use final variable separately)
+            length = length-1;
         }
     }
     printf("Result: %s",str);
