e60e27aa4a4f011e0065e36f4de129f913d3f1e3
harish_selva <harishselvaraj27@gmail.com>
2023-12-12 17:30:32 +0530

    Update Prime Numbers From 1 to N.c

--- Prime Numbers From 1 to N.c
+++ Prime Numbers From 1 to N.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int isprime(int n){
-    if(n==0 || n==1)
+    if(n<=1)
     return 0;
     
-    for(int j=2;j<=n/2;j++){
+    for(int j=2;j*j<=n;j++){
         if(n%j == 0)
         return 0;
     }
@@ -13,11 +13,15 @@ int main() {
     int N;
     printf("Enter the number: ");
     scanf("%d",&N);
+    int count = 0;
+    printf("Prime numbers: ");
     for(int i=1;i<=N;i++){
         if(isprime(i)){
             printf("%d ",i);
+            count++;
         }
     }
+    printf("\nprime number count: %d",count);
 
     return 0;
 }
