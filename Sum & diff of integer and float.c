8ba80e100ae348886cb3c342e9226c70b0767ba1
harish_selva <harishselvaraj27@gmail.com>
2023-12-12 17:42:11 +0530

    Update Sum & diff of integer and float.c

--- Sum & diff of integer and float.c
+++ Sum & diff of integer and float.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include <stdlib.h>
-
 int main()
 {
     int a,b;
     float c,d;
+    printf("Enter two integers:");
     scanf("%d%d",&a,&b);
+    printf("Enter two Floats:");
     scanf("%f%f",&c,&d);
-    printf("%d %d\n",a+b,a-b);
-    printf("%.1f %.1f",c+d,c-d);
+    printf("Integer \nSum: %d  \nDiff: %d\n",a+b,a-b);
+    printf("Float  \nSum: %.1f \nDiff: %.1f",c+d,c-d);
     return 0;
 }
