4ecd562279fab87735f068f2cef7fb860c814b77
harish_selva <harishselvaraj27@gmail.com>
2023-12-16 07:52:40 +0530

    Update 15. Triperfect Number or not.c

--- Examly/15. Triperfect Number or not.c
+++ Examly/15. Triperfect Number or not.c
@@ -1,3 +1,28 @@
+#Naive Approach!
+
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int sum=0;
+    for(int i=1;i<=n;i++){
+        if(n%i==0){
+            sum += i;
+        }
+    }
+    if(sum == n*3){
+        printf("Triperfect Number");
+    }
+    else{
+        printf("Not a Triperfect Number");
+    }
+    return 0;
+}
+
+
+
+#Optimal Approach!
+
 #include<stdio.h>
 #include<math.h>
 int main(){
