c1825081d68d3e1d629b0b99dfef31165b773afe
harish_selva <harishselvaraj27@gmail.com>
2024-01-31 19:45:48 +0530

    Update AutomorphicNumber.c

--- AutomorphicNumber.c
+++ AutomorphicNumber.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 
+int isAutomorphic(int n){
+    int square = n*n;
+    while (n > 0) {
+        if (n % 10 != square % 10) {
+            return 0; // Not an automorphic number
+        }
+        n /= 10;
+        square /= 10;
+    }
+    return 1; 
+}
 int main() {
     int num;
     printf("Enter num: ");
     scanf("%d", &num);
-    int square = (num*num);
-    if (num == square%10) printf("Automorphic number\n");
+    if (isAutomorphic(num)) printf("Automorphic number\n");
     else printf("Not an Automorphic number\n");
     return 0;
 }
