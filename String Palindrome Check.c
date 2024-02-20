d9570083d6bf62c6bcd1f5d7184ffe1236edd756
harish_selva <harishselvaraj27@gmail.com>
2024-02-20 07:06:57 +0530

    Update String Palindrome Check.c

--- String Palindrome Check.c
+++ String Palindrome Check.c
@@ -2,7 +2,7 @@
 #include<string.h>
 int main(){
     char str[100];
-    printf("Enter thet String:");
+    printf("Enter that String:");
     fgets(str,sizeof(str),stdin);
     int length = strlen(str)-1;
     int palin=1;
