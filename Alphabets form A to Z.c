f7a0203a21acf955880f56048f7532135cb21e22
harish_selva <harishselvaraj27@gmail.com>
2024-02-27 21:16:33 +0530

    Update Alphabets form A to Z.c

--- Alphabets form A to Z.c
+++ Alphabets form A to Z.c
@@ -3,7 +3,7 @@
 int main() {
     printf("Alphabets from A to Z: ");
     for(int i='A';i<='Z';i++){
-        printf(" %c",i);
+        printf(" %c",i); 
     }
     printf("\nAlphabets from a to z: ");
     for(int i='a';i<='z';i++){
