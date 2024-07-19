d16e5f2433f8c623e49164b0afdfd3f97283f30e
harish_selva <harishselvaraj27@gmail.com>
2024-07-19 21:52:48 +0530

    Create _03.Anagram.c

---
+++ Talently/_03.Anagram.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main(){
+  long long int n1,n2;
+  scanf("%lld\n%lld",&n1,&n2);
+  int arr1[10]={0};
+  int arr2[10] = {0};
+  while(n1>0){
+    int digit = n1%10;
+    arr1[digit]+=1;
+    n1/=10;
+  }
+  while(n2>0){
+    int digit = n2%10;
+    arr2[digit]+=1;
+    n2/=10;
+  }
+  int flag  = 1;
+  for(int i=0;i<10;i++){
+    if(arr1[i]!=arr2[i]){
+      flag = 0;
+      break;
+    }
+  }
+  if(flag) printf("Anagram");
+  else printf("Not");
+  
+  return 0;
+}
