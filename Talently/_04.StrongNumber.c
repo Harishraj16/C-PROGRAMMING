8760feccc5f36a1142934691ece844b3a9f65e15
harish_selva <harishselvaraj27@gmail.com>
2024-07-19 21:53:53 +0530

    Create _04.StrongNumber.c

---
+++ Talently/_04.StrongNumber.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+long long int factorial(long long int n){
+  if(n==1) return 1;
+  return n*factorial(n-1);
+}
+int main(){
+  long long int n;
+  scanf("%lld",&n);
+  long long int safe = n;
+  long long int sum = 0;
+  while(n>0){
+    int digit = n%10;
+    sum+=factorial(digit);
+    n/=10;
+  }
+  if(sum==safe) printf("Strong Number");
+  else printf("Not a strong number");
+  return 0;
+}
