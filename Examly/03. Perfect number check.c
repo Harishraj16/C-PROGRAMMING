43a5c69e928d051670104264fc8740972cd8e1e0
harish_selva <harishselvaraj27@gmail.com>
2024-07-23 21:52:23 +0530

    Update 03. Perfect number check.c

--- Examly/03. Perfect number check.c
+++ Examly/03. Perfect number check.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 
 int main(){
-    int n;
-    scanf("%d",&n);
+    int n1;
+    scanf("%d",&n1);
     int sum=1;
-    for(int i=2;i<=n/2;i++){
-        if(n%i==0) sum+=i;
+    for(int i=2;i<=n1/2;i++){
+        if(n1%i==0) sum+=i;
     }
-    if(sum==n)
+    if(sum==n1)
         printf("Perfect Number");
     else
         printf("Not a Perfect Number");
